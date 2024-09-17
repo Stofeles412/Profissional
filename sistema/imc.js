5a50c2bd41ec9671e081055272d31129ecd3ef0b
Stofeles412 <fernandostofeles@gmail.com>
2024-09-17 20:52:51 -0300

    .

--- sistema/imc.js
+++ sistema/imc.js
@@ -4,19 +4,32 @@ function callImc() {
     var peso = document.getElementById('peso').value;
     var resposta = document.getElementById('resposta');
 
-    // Convertendo strings para números
+   
     altura = parseFloat(altura);
     peso = parseFloat(peso);
 
-    // Verificando se os valores são números válidos
+    
     if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
         window.alert('Por favor, insira valores válidos para altura e peso.');
         return;
     }
 
-    // Calculando o IMC
+    
     var res = peso / (altura * altura);
 
-    // Exibindo o resultado
+   
     resposta.textContent = `com ${idade} anos de idade, Seu IMC é ${res.toFixed(2)}`;
 }
+
+function verTabela(){
+    var tabela = document.getElementById("tabela")
+    var botao = document.getElementsByClassName("botao")
+    if(tabela.style.display == "none"){
+        tabela.style.display = "inline"
+    }else{
+        tabela.style.display = "none"
+        
+    }
+}
+
+   
