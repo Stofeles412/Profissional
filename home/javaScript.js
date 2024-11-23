a9d989b442519e865287c203a158955ac6d77c3a
Stofeles412 <fernandostofeles@gmail.com>
2024-11-23 16:01:04 -0300

    .

---
+++ home/javaScript.js
@@ -0,0 +1,9 @@
+function Openmenu(){
+var conteudo = document.getElementById("menu-nav")
+var icone = document.getElementById("nav-img")
+if(conteudo.style.display == "none"){
+    conteudo.style.display = "inline-block"
+}else{
+    conteudo.style.display = "none"
+}
+}
