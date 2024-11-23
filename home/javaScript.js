10d08bd9d6f623e1ec1459e80bdfe425449a76f0
Stofeles412 <fernandostofeles@gmail.com>
2024-11-23 16:27:14 -0300

    em desenvolvimento

--- home/javaScript.js
+++ home/javaScript.js
@@ -1,9 +1,11 @@
 function Openmenu(){
-var conteudo = document.getElementById("menu-nav")
-var icone = document.getElementById("nav-img")
+const conteudo = document.getElementById("menu-nav")
+const icone = document.getElementById("icone-menu")
 if(conteudo.style.display == "none"){
     conteudo.style.display = "inline-block"
+    icone.src = "../imagens/divisa-direita.png"
 }else{
     conteudo.style.display = "none"
+      icone.src = "../imagens/divisa-esquerda.png"
 }
 }
