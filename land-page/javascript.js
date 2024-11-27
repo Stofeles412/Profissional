de359eaf9a3421424c7321476a5a1a0669324fc3
Stofeles412 <fernandostofeles@gmail.com>
2024-11-27 11:42:37 -0300

    .

--- land-page/javascript.js
+++ land-page/javascript.js
@@ -1,1 +1,10 @@
 AOS.init();
+function openMenu(){
+const itens = document.getElementById("nav-bar")
+const botao = document.getElementById("botao-menu")
+if (itens.classList.contains("show")){
+    itens.classList.add("show")
+}else{
+    itens.classList.remove("show")
+}
+}
