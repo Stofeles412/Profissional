941bbb2a39cf0efeaab8681a89d28e62d1b243cf
Stofeles412 <fernandostofeles@gmail.com>
2025-01-28 15:18:24 -0300

    .

--- portifólio/card.js
+++ portifólio/card.js
@@ -1,10 +1,5 @@
 function openMenu(){
     var abrir = document.getElementById("menu")
     var botao = document.getElementById("Botao-menu")
-    if (abrir.classList.contains("Show")){
-        abrir.classList.remove("Show")
-    }else{
-        abrir.classList.add("Show")
-        
-    }
+    abrir.classList.toggle("show")
 }
